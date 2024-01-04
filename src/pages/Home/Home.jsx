4f42aab1bd71a6c7c7239a39f364a0418ef36eaa
andressa-leite <dre_tl@hotmail.com>
2024-01-04 18:45:31 -0300

    correções gerais

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import NavBar from "../../components/NavBar/NavBar";
 import Card from "../../components/Card/Card";
-//import { news } from "../../Data";
 import { HomeBody } from "./HomeStyled";
 import { getAllPosts } from "../../services/postsService";
 
--- src/services/postsService.js
+++ src/services/postsService.js
@@ -3,6 +3,6 @@ import axios from "axios";
 const baseURL = "http://localhost:3000";
 
 export function getAllPosts(){
-    const response = axios.get(`${baseURL}/posts`)
+    const response = axios.get(`${baseURL}/news`)
     return response;
 }
